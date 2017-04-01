bfbc6398285c8fe271b482856d443bf6c2ad2088
PavelPivkin <dreamer527@yandex.ru>
2017-04-01 16:25:54 +0300

    simple login/logout added

---
+++ GetAppBackend/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(64)
+
+import bank
+import application
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='GetAppBackend',
+    packages=['GetAppBackend'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
